e7127a34f132b96f2a37e29ddee3f95866bd648b
Mascagni-ML <tatramario@gmail.com>
2025-07-07 10:27:14 +0200

    New version of Dean Model - tst02

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IHSetDean91',
+    version='2.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'pathlib',
+        'matplotlib'
+    ],
+    author='Mario Luiz Mascagni',
+    author_email='tatramario@gmail.com',
+    description='IH-SET Dean (1991)',
+    url='https://github.com/IHCantabria/IHSetDean',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+		'Programming Language :: Python :: 3.11',
+    ],
+)
+
