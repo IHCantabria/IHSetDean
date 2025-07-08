2857f1d8ceff352d773106613d941be49a7d74b2
Mascagni-ML <tatramario@gmail.com>
2025-07-08 12:32:09 +0200

    new Dean model with direct_run A corrected

--- IHSetDean91/__init__.py
+++ IHSetDean91/__init__.py
@@ -2,5 +2,5 @@
 
 # Import modules and functions from your package here
 from .dean1991 import dean1991
-from .calibration import cal_Dean91_2
-from .direct_run import dean1991_run
+from .calibration import cal_dean1991
+
