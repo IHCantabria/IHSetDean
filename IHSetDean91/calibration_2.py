9b2bbf6890a8aedb0e66e0459f8de7b611e41d3d
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-07-10 12:02:25 +0200

    Correcting initialization

--- IHSetDean91/calibration_2.py
+++ IHSetDean91/calibration_2.py
@@ -5,22 +5,23 @@ from .dean1991 import dean1991, dean1991_rev
 
 class cal_dean1991_2(object):  
 
-    def __init__(self, sl: float = 0.0, doc: float = 10.0):
+    def __init__(self, sl, doc, d50):
         
-        self.sl = sl        # sea level 
-        self.doc = doc      # depth of closure
-        self.A = None       # Dean's parameter (m\ :sup:`1/3`)
-        self.D50 = None     # median grain size (mm)
-        self.x_obs = None   # observed cross-shore distance (m)
-        self.y_obs = None   # observed profile elevation (m, positive upwards)
-        self.data = False   # flag to check if data is loaded
+        self.sl = sl
+        self.doc = doc
+        self.A = None
+        self.D50 = d50
+        self.x_obs = None
+        self.y_obs = None
+        self.data = False
 
     @staticmethod
     def _A_from_d50(d50_mm: float) -> float:
-        return (0.067 * ((d50_mm) ** 0.44))
+        return 0.067 * ((d50_mm) ** 0.44)
 
     def from_d50(self, d50_mm: float) -> tuple[np.ndarray, np.ndarray]:
         self.d50 = d50_mm
+   
         if self.data:
             return self.dean_A(self._A_from_d50(d50_mm))
         else:
@@ -36,6 +37,7 @@ class cal_dean1991_2(object):
     def change_sl(self, sl: float):
         self.sl = sl
         if self.data:
+            self.recalc_x_dense()
             return self.dean_A(self.A)
         else:
             return self.dean_A_rev(self.A)
@@ -49,7 +51,6 @@ class cal_dean1991_2(object):
             return self.dean_A_rev(self.A)
             
     def add_data(self, path):
-        
         df = pd.read_csv(path, header=None, names=["X", "Y"])
         self.x_raw = df["X"].to_numpy(float)
         self.y_raw = df["Y"].to_numpy(float)
@@ -67,10 +68,9 @@ class cal_dean1991_2(object):
         self.y_obs_rel = self.y_obs - self.sl  # relative to SL
 
         # Initialize main variables
-        self.x_dense = np.linspace(float(self.x_obs.min()), float(self.x_obs.max()), 1e+3)
+        self.x_dense = np.linspace(float(self.x_obs.min()), float(self.x_obs.max()), 1000)
 
         self.data = True
-
         return self.calibrate()
 
     def dean_A_rev(self, A: float) -> tuple[np.ndarray, np.ndarray]:
@@ -78,6 +78,7 @@ class cal_dean1991_2(object):
         self.A = A
         y = np.linspace(self.sl, self.doc + 0.5, 1000)
         x = dean1991_rev(y, A, self.sl)
+
         return x, y
     
     def dean_A(self, A: float) -> tuple[np.ndarray, np.ndarray]:
@@ -92,7 +93,7 @@ class cal_dean1991_2(object):
 
     def calibrate(self):
         guess = 0.067 * (0.3) ** 0.44  # fixed seed (~D50 = 0.30 mm)
-        [A_opt] = fsolve(self._grad_sse, x0=guess, xtol=1e-6, maxfev=5e+3)
+        [A_opt] = fsolve(self._grad_sse, x0=guess, xtol=1e-6, maxfev=5000)
         return self.dean_A(float(A_opt))
         
     def recalc_x_dense(self):
