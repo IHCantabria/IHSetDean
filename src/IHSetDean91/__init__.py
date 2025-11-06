e9584a47ef7a4b2b718ead009dbd55f21ad5315e
mmascagni <tatramario@gmail.com>
2025-11-06 12:42:25 +0100

    ajuste de paths em Dean para que seja similar a bernabeu

--- src/IHSetDean91/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/__init__.py
-
-# Import modules and functions from your package here
-from .dean1991 import dean1991
-from .calibration import cal_dean1991
-#from .calibration import cal_dean1991
-
